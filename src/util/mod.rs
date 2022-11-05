25be2b9a7a23d50f069dbd9fb79a96aeda5c7f0c
Ryan Sivek <nanowizardry@gmail.com>
2022-11-05 09:56:56 -0400

    add sigmoid function

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,3 +1,4 @@
+use std::f64::consts::E;
 pub mod quantity_wrapper;
 pub mod id_gen;
 pub mod version;
@@ -5,3 +6,14 @@ pub mod version;
 pub use quantity_wrapper::*;
 pub use id_gen::{IdGenerator, IdType};
 pub use version::Version;
+
+/// The mathematical sigmoid / logistic function with additional bounds
+/// to define function shape
+/// 
+/// ### Arguments
+/// * `t` - time
+/// * `d` - duration
+/// * `a` - amplitude
+fn bound_sigmoid(t: &f64, d: &f64, a: &f64) -> f64 {
+    return a / (1.0 + f64::powf(E, ((4.0 * E) / d) * t - 2.0 * E))
+}
