ab95a40e2fa404a5795890f06ea9dc56dbdca63c
Ryan Sivek <nanowizardry@gmail.com>
2024-03-12 09:48:08 -0400

    add sync test

--- src/sim/layer/digestion/digestion_layer.rs
+++ src/sim/layer/digestion/digestion_layer.rs
@@ -264,9 +264,9 @@ impl<O: Organism, T: DigestionComponent<O>> SimComponentProcessorSync<O, T> for 
 
 #[cfg(test)]
 mod tests {
-    use std::{ops::RangeBounds, sync::Arc};
+    use std::{borrow::BorrowMut, ops::RangeBounds, sync::{Arc, Mutex}, thread::scope};
 
-    use crate::{sim::{component::{SimComponent, SimComponentProcessor}, layer::{digestion::{component::test::TestDigestionComponent, consumable::test::{test_ammonia, test_fiber, test_food}, ConsumeEvent, DigestionDirection, EliminateEvent}, InternalLayerTrigger, SimLayer}, organism::test::TestOrganism, SimConnector, SimTime}, substance::{Substance, SubstanceConcentration}, util::secs};
+    use crate::{sim::{component::{SimComponent, SimComponentProcessor, SimComponentProcessorSync}, layer::{digestion::{component::test::TestDigestionComponent, consumable::test::{test_ammonia, test_fiber, test_food}, ConsumeEvent, DigestionComponent, DigestionDirection, EliminateEvent}, InternalLayerTrigger, SimLayer}, organism::test::TestOrganism, Organism, SimConnector, SimTime}, substance::{Substance, SubstanceConcentration}, util::secs};
 
     use super::DigestionLayer;
 
@@ -274,6 +274,26 @@ mod tests {
     #[test]
     fn layer() {
         DigestionLayer::<TestOrganism>::new();
+    }
+
+    fn run_layer<O: Organism>(layer: &mut DigestionLayer<O>, connector: &mut SimConnector, components: &mut Vec<TestDigestionComponent<O>>) {
+        for component in components.iter_mut() {
+            layer.prepare_component(connector, component);
+            component.run();
+            layer.process_component(connector, component);
+        }
+    }
+
+    fn run_layer_sync<O: Organism>(layer: &Mutex<DigestionLayer<O>>, connector: &Mutex<SimConnector>, components: &mut Vec<TestDigestionComponent<O>>) {
+        scope(|s| {
+            for component in components.iter_mut() {
+                s.spawn(|| {
+                    layer.lock().unwrap().prepare_component_sync(&mut connector.lock().unwrap(), component);
+                    component.run();
+                    layer.lock().unwrap().process_component_sync(&mut connector.lock().unwrap(), component);
+                });
+            }
+        });
     }
 
     #[test]
@@ -296,21 +316,13 @@ mod tests {
         connector.active_events.push(Arc::new(ConsumeEvent(test_fiber(150.0))));
 
         // Initial run (nothing should happen)
-        for component in components.iter_mut() {
-            layer.prepare_component(&mut connector, component);
-            component.run();
-            layer.process_component(&mut connector, component);
-        }
+        run_layer(&mut layer, &mut connector, &mut components);
 
         // First pre_exec should pull in the 3 ConsumeEvents
         layer.pre_exec(&mut connector);
         assert!(layer.consumed_map.get(0).is_some_and(|x| x.len() == 3));
 
-        for component in components.iter_mut() {
-            layer.prepare_component(&mut connector, component);
-            component.run();
-            layer.process_component(&mut connector, component);
-        }
+        run_layer(&mut layer, &mut connector, &mut components);
 
         layer.post_exec(&mut connector);
 
@@ -328,13 +340,7 @@ mod tests {
         assert!(next_events.next().is_none());
 
         layer.pre_exec(&mut connector);
-
-        for component in components.iter_mut() {
-            layer.prepare_component(&mut connector, component);
-            component.run();
-            layer.process_component(&mut connector, component);
-        }
-        
+        run_layer(&mut layer, &mut connector, &mut components);
         layer.post_exec(&mut connector);
 
         connector.time_manager.advance_by(SimTime::from_s(2.0));
@@ -367,11 +373,7 @@ mod tests {
         assert!(layer.consumed_map.get(0).unwrap().get(0).unwrap().concentration_of(&Substance::GLC) < starting_glc);
         
         // Exec for completeness (shouldn't change anything)
-        for component in components.iter_mut() {
-            layer.prepare_component(&mut connector, component);
-            component.run();
-            layer.process_component(&mut connector, component);
-        }
+        run_layer(&mut layer, &mut connector, &mut components);
         layer.post_exec(&mut connector);
 
         connector.time_manager.advance_by(SimTime::from_min(1.0));
@@ -384,11 +386,7 @@ mod tests {
         assert_eq!(layer.consumed_map.get(1).unwrap().len(), 1);
         assert!(layer.consumed_map.get(1).unwrap().get(0).unwrap().concentration_of(&Substance::Cellulose) > SubstanceConcentration::from_mM(0.0));
 
-        for component in components.iter_mut() {
-            layer.prepare_component(&mut connector, component);
-            component.run();
-            layer.process_component(&mut connector, component);
-        }
+        run_layer(&mut layer, &mut connector, &mut components);
         layer.post_exec(&mut connector);
 
         // each component should have 1 consumed
@@ -398,11 +396,7 @@ mod tests {
         // Go through a cycle
         connector.time_manager.advance_by(SimTime::from_min(5.0));
         layer.pre_exec(&mut connector);
-        for component in components.iter_mut() {
-            layer.prepare_component(&mut connector, component);
-            component.run();
-            layer.process_component(&mut connector, component);
-        }
+        run_layer(&mut layer, &mut connector, &mut components);
         layer.post_exec(&mut connector);
         
         // Each list should be empty now
@@ -427,28 +421,127 @@ mod tests {
 
     }
 
-    // #[test]
-    // fn layer_process_sync() {
-    //     let layer = Mutex::new(CirculationLayer::<TestOrganism>::new());
-    //     let connector = Mutex::new(SimConnector::new());
-    //     let mut component = TestCircComponentA::new();
-    //     layer.lock().unwrap().setup_component_sync(&mut connector.lock().unwrap(), &mut component);
+    #[test]
+    fn layer_process_sync() {
+        let layer = Mutex::new(DigestionLayer::<TestOrganism>::new());
+        let connector = Mutex::new(SimConnector::new());
+        let mut components = vec![
+            TestDigestionComponent::new(),
+            TestDigestionComponent::new(),
+        ];
+        for component in components.iter_mut() {
+            layer.lock().unwrap().setup_component_sync(&mut connector.lock().unwrap(), component);
+        }
 
-    //     component
-    //         .circulation_connector()
-    //         .vessel_map_sync
-    //         .insert(TestBloodVessel::VenaCava, Arc::new(Mutex::new(BloodStore::new())));
+        let food = test_food(200.0);
+        let starting_glc = food.concentration_of(&Substance::GLC);
 
-    //     layer.lock().unwrap().pre_exec_sync(&mut connector.lock().unwrap());
+        connector.lock().unwrap().active_events.push(Arc::new(ConsumeEvent(food)));
+        connector.lock().unwrap().active_events.push(Arc::new(ConsumeEvent(test_ammonia(50.0))));
+        connector.lock().unwrap().active_events.push(Arc::new(ConsumeEvent(test_fiber(150.0))));
 
-    //     scope(|s| {
-    //         s.spawn(|| {
-    //             layer.lock().unwrap().prepare_component_sync(&mut connector.lock().unwrap(), &mut component);
-    //             component.run();
-    //             layer.lock().unwrap().process_component_sync(&mut connector.lock().unwrap(), &mut component);
-    //         });
-    //     });
+        // Initial run (nothing should happen)
+        run_layer_sync(&layer, &connector, &mut components);
 
-    //     layer.lock().unwrap().post_exec_sync(&mut connector.lock().unwrap());
-    // }
+        // First pre_exec should pull in the 3 ConsumeEvents
+        layer.lock().unwrap().pre_exec(&mut connector.lock().unwrap());
+        assert!(layer.lock().unwrap().consumed_map.get(0).is_some_and(|x| x.len() == 3));
+
+        run_layer_sync(&layer, &connector, &mut components);
+
+        layer.lock().unwrap().post_exec(&mut connector.lock().unwrap());
+
+        // Make sure to drain the active events so they don't keep
+        // getting added
+        connector.lock().unwrap().active_events.drain(..);
+
+        connector.lock().unwrap().time_manager.advance_by(SimTime::from_s(10.0));
+
+        // Should schedule an internal trigger event
+        let mut next_events = connector.lock().unwrap().time_manager.next_events();
+        assert!(next_events.next().unwrap().1.get(0).unwrap().is::<InternalLayerTrigger>());
+
+        // Shouldn't see any EliminateEvents yet
+        assert!(next_events.next().is_none());
+
+        layer.lock().unwrap().pre_exec(&mut connector.lock().unwrap());
+        run_layer_sync(&layer, &connector, &mut components);
+        layer.lock().unwrap().post_exec(&mut connector.lock().unwrap());
+
+        connector.lock().unwrap().time_manager.advance_by(SimTime::from_s(2.0));
+
+        // Should be eliminating the ammonia
+        let (exec_time, mut evts) = connector.lock().unwrap().time_manager.next_events().next().unwrap();
+        let expected_time = SimTime::from_s(10.0) + DigestionLayer::<TestOrganism>::ELIMINATION_DELAY;
+        let threshold = secs!(0.1);
+        assert!(
+            (exec_time-threshold..exec_time+threshold).contains(&expected_time),
+            "Expected time {} != {}",
+            expected_time,
+            exec_time
+        );
+
+        assert!(evts.get(0).is_some());
+        assert!(evts.get(0).unwrap().is::<EliminateEvent>());
+
+        let elim = evts.pop().unwrap().downcast::<EliminateEvent>().unwrap();
+        assert_eq!(elim.direction, DigestionDirection::BACK);
+        assert!(elim.excrement.concentration_of(&Substance::NH3) > SubstanceConcentration::from_mM(0.0));
+
+        layer.lock().unwrap().pre_exec(&mut connector.lock().unwrap());
+
+
+        // Both of the remaining consumables should still be with the first component
+        assert_eq!(layer.lock().unwrap().consumed_map.get(0).unwrap().len(), 2);
+        
+        // Should be consuming some sugar
+        assert!(layer.lock().unwrap().consumed_map.get(0).unwrap().get(0).unwrap().concentration_of(&Substance::GLC) < starting_glc);
+        
+        // Exec for completeness (shouldn't change anything)
+        run_layer_sync(&layer, &connector, &mut components);
+        layer.lock().unwrap().post_exec(&mut connector.lock().unwrap());
+
+        connector.lock().unwrap().time_manager.advance_by(SimTime::from_min(1.0));
+        layer.lock().unwrap().pre_exec(&mut connector.lock().unwrap());
+
+        // First component should still have the food
+        assert_eq!(layer.lock().unwrap().consumed_map.get(0).unwrap().len(), 1);
+
+        // Fiber should have moved to the next component
+        assert_eq!(layer.lock().unwrap().consumed_map.get(1).unwrap().len(), 1);
+        assert!(layer.lock().unwrap().consumed_map.get(1).unwrap().get(0).unwrap().concentration_of(&Substance::Cellulose) > SubstanceConcentration::from_mM(0.0));
+
+        run_layer_sync(&layer, &connector, &mut components);
+        layer.lock().unwrap().post_exec(&mut connector.lock().unwrap());
+
+        // each component should have 1 consumed
+        assert_eq!(layer.lock().unwrap().consumed_map.get(0).unwrap().len(), 1);
+        assert_eq!(layer.lock().unwrap().consumed_map.get(1).unwrap().len(), 1);
+
+        // Go through a cycle
+        connector.lock().unwrap().time_manager.advance_by(SimTime::from_min(5.0));
+        layer.lock().unwrap().pre_exec(&mut connector.lock().unwrap());
+        run_layer_sync(&layer, &connector, &mut components);
+        layer.lock().unwrap().post_exec(&mut connector.lock().unwrap());
+        
+        // Each list should be empty now
+        assert_eq!(layer.lock().unwrap().consumed_map.get(0).unwrap().len(), 0);
+        assert_eq!(layer.lock().unwrap().consumed_map.get(1).unwrap().len(), 0);
+
+        connector.lock().unwrap().time_manager.advance_by(SimTime::from_s(1.0));
+
+        // Should be eliminating the fiber
+        let (_, mut evts) = connector.lock().unwrap().time_manager.next_events().next().unwrap();
+
+        assert!(evts.get(0).is_some());
+        assert!(evts.get(0).unwrap().is::<EliminateEvent>());
+
+        let elim = evts.pop().unwrap().downcast::<EliminateEvent>().unwrap();
+        assert_eq!(elim.direction, DigestionDirection::FORWARD);
+        assert!(elim.excrement.concentration_of(&Substance::Cellulose) > SubstanceConcentration::from_mM(0.0));
+
+        // Food should have dissappeared
+        assert!(layer.lock().unwrap().consumed_map.get(0).unwrap().is_empty());
+        assert!(layer.lock().unwrap().consumed_map.get(1).unwrap().is_empty());
+    }
 }
