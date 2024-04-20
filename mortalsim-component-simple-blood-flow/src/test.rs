62ed23c3684eee8364dd594406ad47d81a5d4728
Ryan Sivek <nanowizardry@gmail.com>
2024-04-20 18:51:16 -0400

    added blood coponent harness for testing

--- mortalsim-component-simple-blood-flow/src/test.rs
+++ mortalsim-component-simple-blood-flow/src/test.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
 use mortalsim_core::sim::component::SimComponent;
 use mortalsim_core::sim::layer::circulation::{CirculationComponent, CirculationConnector};
@@ -11,8 +11,8 @@ use rand::distributions::{Alphanumeric, DistString};
 struct TestBloodComponent {
     id: &'static str,
     vessel: TestBloodVessel,
-    pending_writes: Vec<(SimTime, Substance, SubstanceChange)>,
-    pending_reads: Vec<(SimTime, Substance)>,
+    pending_writes: VecDeque<(SimTime, Substance, SubstanceChange)>,
+    pending_reads: VecDeque<(SimTime, Substance)>,
     reads: Vec<(SimTime, Substance, SubstanceConcentration)>,
     circ_connector: CirculationConnector<TestOrganism>,
 }
@@ -23,17 +23,21 @@ impl TestBloodComponent {
         mut writes: Vec<(SimTime, Substance, SubstanceChange)>,
         mut reads: Vec<(SimTime, Substance)>,
     ) -> Self {
-        writes.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
-        reads.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+        writes.sort_by(|a, b| a.0.cmp(&b.0));
+        reads.sort_by(|a, b| a.0.cmp(&b.0));
 
         Self {
             id: Alphanumeric.sample_string(&mut rand::thread_rng(), 16).leak(),
             vessel,
-            pending_writes: writes,
-            pending_reads: reads,
+            pending_writes: writes.into(),
+            pending_reads: reads.into(),
             reads: Vec::new(),
             circ_connector: CirculationConnector::new(),
         }
+    }
+
+    fn get_reads(&self) -> impl Iterator<Item = &(SimTime, Substance, SubstanceConcentration)> {
+        self.reads.iter()
     }
 }
 
@@ -64,6 +68,25 @@ impl SimComponent<TestOrganism> for TestBloodComponent {
     }
 
     fn run(&mut self) {
-        
+        let sim_time = self.circ_connector.sim_time();
+
+        // See if we need to write any substance changes
+        while self.pending_writes.len() > 0 && self.pending_writes.get(0).unwrap().0 < sim_time {
+            let (_, substance, change) = self.pending_writes.pop_front().unwrap();
+            self.circ_connector
+                .blood_store(&self.vessel)
+                .unwrap()
+                .schedule_custom_change(substance, change);
+        }
+
+        while self.pending_reads.len() > 0 && self.pending_reads.get(0).unwrap().0 < sim_time {
+            let (_, substance) = self.pending_reads.pop_front().unwrap();
+            let conc = self.circ_connector
+                .blood_store(&self.vessel)
+                .unwrap()
+                .concentration_of(&substance);
+
+            self.reads.push((sim_time, substance, conc));
+        }
     }
 }
